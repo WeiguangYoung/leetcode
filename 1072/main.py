c740f20d65f1aa03cb1dca36f91e0822ac6518c6
yangweiguang <yangweiguang@uniml.com>
2023-05-15 12:04:27 +0800

    init 1072

---
+++ 1072/main.py
@@ -0,0 +1,13 @@
+'''
+Author: yangweiguang yangweiguang@uniml.com
+Date: 2023-05-15 12:03:39
+LastEditors: yangweiguang yangweiguang@uniml.com
+LastEditTime: 2023-05-15 12:03:42
+FilePath: /leetcode/1072/main.py
+Description: 
+
+Copyright (c) 2023 by GuangjiTech, All Rights Reserved. 
+'''
+class Solution:
+    def maxEqualRowsAfterFlips(self, matrix: List[List[int]]) -> int:
+        pass
