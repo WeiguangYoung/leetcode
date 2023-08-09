5988e7a62b7094d61e8f36904f651ad7a27b1a0d
yangweiguang <727554434@qq.com>
2023-08-09 10:09:27 +0800

    add offer/20

---
+++ offer/20/main.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isNumber(self, s: str) -> bool:
+        states =[
+            {' ': 0, 's': 1, 'd': 2, '.': 4},
+            {'d': 2, '.': 4},
+            {'d': 2, '.': 3, 'e': 5, ' ': 8},
+            {'d': 3, 'e': 5, ' ': 8},
+            {'d': 3},
+            {'d': 7, 's': 6},
+            {'d': 7},
+            {'d': 7, ' ': 8},
+            {' ': 8},
+        ]
+        p = 0
+        for c in s:
+            if c == " ":
+                t = " "
+            elif c == ".":
+                t = "."
+            elif c in "Ee":
+                t = "e"
+            elif c in "+-":
+                t = "s"
+            elif "0" <= c <= "9":
+                t = "d"
+            else:
+                t = "?"
+            if t not in states[p]:
+                return False
+            p = states[p][t]
+        return p in [2, 3, 7, 8]
