1867ea4b7488504e916945849f624d5601b575a5
yang <727554434@qq.com>
2023-08-13 19:00:25 +0800

    fix offer 59-2

--- offer/59-2/main.py
+++ offer/59-2/main.py
@@ -1,40 +1,42 @@
+import collections
+
 class MaxQueue:
     def __init__(self):
-        self.queue = []
-        self.max_queue = []
+        self.queue = collections.deque()
+        self.max_queue = collections.deque()
 
     def max_value(self) -> int:
-        if self.max_queue:
-            return self.max_queue[-1]
-        return -1
+        if not self.max_queue:
+            return -1
+        return self.max_queue[0]
 
     def push_back(self, value: int) -> None:
         self.queue.append(value)
 
-        if not self.max_queue:
-            self.max_queue.append(value)
-        elif self.max_queue[-1] <= value:
-            self.max_queue.append(value)
+        while self.max_queue and self.max_queue[-1] < value:
+            self.max_queue.pop()
+        self.max_queue.append(value)
 
     def pop_front(self) -> int:
         if not self.queue:
             return -1
 
-        if self.max_queue and self.max_queue[-1] == self.queue[0]:
-            self.max_queue = self.max_queue[:-1]
+        if self.max_queue and self.max_queue[0] == self.queue[0]:
+            self.max_queue.popleft()
 
-        value = self.queue[0]
-        self.queue = self.queue[1:]
+        value = self.queue.popleft()
         return value
 
 
 # Your MaxQueue object will be instantiated and called as such:
 obj = MaxQueue()
-obj.push_back(96)
-obj.push_back(16)
-obj.push_back(89)
+obj.push_back(1)
+obj.push_back(2)
+obj.push_back(2)
+print(obj.max_value())
 param_2 = obj.pop_front()
 print(param_2)
-param_1 = obj.push_back(22)
+# param_1 = obj.push_back(22)
 param_3 = obj.pop_front()
 print(param_3)
+print(obj.max_value())
