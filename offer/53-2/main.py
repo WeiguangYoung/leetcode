cdcddfc42200c299b58c7adbac8db4ebbf54d1dd
yang <727554434@qq.com>
2023-08-23 18:56:38 +0800

    add offer/04

--- offer/53-2/main.py
+++ offer/53-2/main.py
@@ -2,4 +2,23 @@ from typing import List
 
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        pass
+        left, right = 0, len(nums) - 1
+        
+        if len(nums)==1:
+            if nums[0]==1:
+                return 0
+            else:
+                return 1
+        
+        while left<right:
+            mid = (left+right) // 2
+            if nums[mid]==mid:
+                left = mid+1
+            else:
+                right = mid-1
+        
+        return left
+    
+s = Solution()
+r=s.missingNumber([0])
+print(r)
