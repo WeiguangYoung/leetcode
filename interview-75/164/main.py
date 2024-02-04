bb7c5abd32143e102b9d926dd82501f24035068a
yangweiguang <727554434@qq.com>
2024-02-04 10:12:33 +0800

    update

--- interview-75/164/main.py
+++ interview-75/164/main.py
@@ -8,11 +8,16 @@ class Solution:
                 return
             i, j = l, r
             while i < j:
+                # 从最后一个向前比较，比较对象为l
                 while strs[j] + strs[l] >= strs[l] + strs[j] and i < j:
                     j -= 1
+                # 从第一个向前比较，比较对象为l
                 while strs[i] + strs[l] <= strs[l] + strs[i] and i < j:
                     i += 1
+
+                # 交换i和j的位置
                 strs[i], strs[j] = strs[j], strs[i]
+
             strs[i], strs[l] = strs[l], strs[i]
             quick_sort(l, i - 1)
             quick_sort(i + 1, r)
@@ -23,5 +28,5 @@ class Solution:
 
 
 s = Solution()
-result = s.crackPassword([15, 8, 7])
+result = s.crackPassword([6, 5, 4, 3, 2, 1])
 print(result)
