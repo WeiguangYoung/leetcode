0192e9489fbb2de38d95eaeccb7836ebd64532e0
yangweiguang <727554434@qq.com>
2024-03-05 21:48:49 +0800

    add pandas-30 1693

--- pandas-30/1693/main.sql
+++ pandas-30/1693/main.sql
@@ -2,7 +2,7 @@ SELECT
     date_id,
     make_name,
     COUNT(DISTINCT lead_id) AS unique_leads,
-    COUNT(DISTINCT partner_id) AS unique_partners
+    COUNT(DISTINCT unique_partners) AS unique_partners,
 FROM
     DailySales
 GROUP BY
