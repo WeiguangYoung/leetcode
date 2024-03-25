a41c880622200970d5c9bdedd240d33814940d8c
yangweiguang <727554434@qq.com>
2024-03-25 20:32:44 +0800

    add database 620

---
+++ database/620/main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+
+def not_boring_movies(cinema: pd.DataFrame) -> pd.DataFrame:
+    cinema = cinema[cinema["id"] %
+                    2 == 1 & ~(cinema["description"] == "boring")]
+    cinema = cinema.sort_values("rating", ascending=False)
+    return cinema
