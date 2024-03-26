1ea446b28fadf09cd820cfc70a021e797c9041d0
yangweiguang <727554434@qq.com>
2024-03-26 21:01:11 +0800

    add lcr 149

---
+++ lcr/149/main.py
@@ -0,0 +1,25 @@
+import collections
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def decorateRecord(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
+        res, queue = [], collections.deque()
+        queue.append(root)
+        while queue:
+            node = queue.popleft()
+            res.append(node.val)
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        return res
