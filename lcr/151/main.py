673594ca6643e6e4c0c1ce085f5a47c96b86717c
yangweiguang <727554434@qq.com>
2024-03-26 21:06:00 +0800

    add lcr 151

---
+++ lcr/151/main.py
@@ -0,0 +1,30 @@
+import collections
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def decorateRecord(self, root: TreeNode) -> List[List[int]]:
+        if not root:
+            return []
+        res, deque = [], collections.deque([root])
+        while deque:
+            tmp = collections.deque()
+            for _ in range(len(deque)):
+                node = deque.popleft()
+                if len(res) % 2 == 0:
+                    tmp.append(node.val)  # 奇数层 -> 插入队列尾部
+                else:
+                    tmp.appendleft(node.val)  # 偶数层 -> 插入队列头部
+                if node.left:
+                    deque.append(node.left)
+                if node.right:
+                    deque.append(node.right)
+            res.append(list(tmp))
+        return res
