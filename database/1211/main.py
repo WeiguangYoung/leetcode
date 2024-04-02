7498b90263eb4be215e8dd293e224c12891da2ea
yangweiguang <727554434@qq.com>
2024-04-02 09:30:21 +0800

    add database 1211

---
+++ database/1211/main.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+
+def queries_stats(queries: pd.DataFrame) -> pd.DataFrame:
+    queries["quality"] = queries["rating"] / queries["position"]
+    queries["poor_query_percentage"] = queries["rating"].apply(
+        lambda x: 1 if x < 3 else 0)
+    grouped_queries = queries.groupby("query_name")
+    grouped_queries = grouped_queries.agg({
+        "quality": lambda x: round(x.mean(), 2),
+        "poor_query_percentage": lambda x: round(x.sum()*100/x.count(), 2),
+    }).reset_index()
+    return grouped_queries
