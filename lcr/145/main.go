1813016fb6fc97136d3a43777cf06dbb5e86cfa1
yangweiguang <727554434@qq.com>
2024-04-04 14:43:18 +0800

    add lcr 145

---
+++ lcr/145/main.go
@@ -0,0 +1,31 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isSymmetric(root *TreeNode) bool {
+	queue := []*TreeNode{
+		root,
+		root,
+	}
+	for len(queue) > 0 {
+		a, b := queue[0], queue[1]
+		queue = queue[2:]
+
+		if a == nil && b == nil {
+			continue
+		}
+		if a == nil || b == nil {
+			return false
+		}
+		if a.Val != b.Val {
+			return false
+		}
+
+		queue = append(queue, a.Left, b.Right, b.Left, a.Right)
+	}
+	return true
+}
