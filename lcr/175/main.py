98f3374559de98a9ad35882904626f85fc28a5ce
yangweiguang <727554434@qq.com>
2024-04-14 14:54:30 +0800

    add lcr 175

--- lcr/175/main.py
+++ lcr/175/main.py
@@ -1,18 +1,4 @@
-from typing import List
-
-
 class Solution:
-    def trainingPlan(self, actions: List[int]) -> int:
-        counts = [0]*32
-        for action in actions:
-            for i in range(32):
-                counts[i] += action & 1
-                action >>= 1
-        
-        res = 0
-        for i in range(32):
-            res <<= 1
-            res |= counts[i]%3
-        
-        return res
-
+    def calculateDepth(self, root: TreeNode) -> int:
+        if not root: return 0
+        return max(self.calculateDepth(root.left), self.calculateDepth(root.right)) + 1
